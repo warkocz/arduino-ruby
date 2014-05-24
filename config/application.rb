8124987c7e6831f1df1872db31628d8bb45c4928
Tomasz Warkocki <warkocz@codequest.com>
2014-05-24 23:44:39 +0200

    fixing bootsrap fonts issues

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,7 @@ module Arduino
     # -- all .rb files in that directory are automatically loaded.
     config.autoload_paths += Dir[Rails.root.join('app/services', '{**/}')]
 
+    config.assets.paths << "#{Rails}/vendor/assets/fonts"
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
